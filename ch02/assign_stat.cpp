22372ea0e9086c904b4e3b4ae874b6873c924eaf
Keshawn <skeshawn86@gmail.com>
2025-02-12 21:07:55 -0500

    Update ch02 code

---
+++ ch02/assign_stat.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c;
+
+    a = pow(a, 2);
+
+    cout << a << endl;
+
+    return 0;
+}
