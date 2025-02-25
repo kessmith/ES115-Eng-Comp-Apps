0671deef28af90eaf566187a05b274e7cd5214dd
Keshawn Smith <keshawnsmith@Keshawns-MacBook-Pro.local>
2025-02-25 12:43:13 -0500

    Chapter 2 and 3 Code

--- ch02/assign_pp.cpp
+++ ch02/assign_pp.cpp
@@ -11,13 +11,68 @@ int main() {
 }
 */
 
+// int main() {
+//     int counter = 10;
+//     int original_count = counter; 
+
+//     counter--;
+
+
+//     cout << "Original Count: " << original_count << " New count is: " << counter << endl;
+//     return 0;
+// }
+
+// int main() {
+//     int x = 40; 
+//     int y = 3;
+
+//     int divide = x/y;
+//     int remainder = x % 100;
+
+//     cout << "Division Answer is: " << divide << "Remainder is: " << remainder << endl;
+//     return 0;
+// }
+
+// int main() {
+//     int bottles;
+//     int cans;
+
+//     cout << "Enter the number of bottles: ";
+//     cin >> bottles;
+
+//     cans = bottles * 12;
+//     cout << "You have " << cans << " cans" << endl;
+//     return 0;
+// }
+
+// int main() {
+//     string myName;
+//     cout << "Enter your name: ";
+//     cin >> myName;
+
+//     cout << "Hello World!, My name is " << myName << endl;
+// }
+
 int main() {
-    int counter = 10;
+    int floor, actual_floor;
 
-    counter--;
+    cout << "Enter desired floor number: ";
+    cin >> floor;
 
-    int original_count = counter; 
+    actual_floor = floor;
 
-    cout << "Original Count: " << original_count << " New count is: " << counter << endl;
+    if (floor > 13) {
+        actual_floor--; // decrement by 1
+    }
+    cout << "You are going to floor " << actual_floor << endl;
     return 0;
+
+    // if(floor > 12 or floor == 13) {
+    //     actual_floor = floor - 1;
+    //     cout << "You are going to floor " << actual_floor << endl;
+    //     return 0;
+    // } else {
+    //     cout << "You are on floor " << floor << endl;
+    //     return 0;
+    // }
 }
