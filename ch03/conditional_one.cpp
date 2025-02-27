3a89301e312994a99c6427bb86e6741ec4084f5f
Keshawn Smith <keshawnsmith@Keshawns-MacBook-Pro.local>
2025-02-27 09:27:12 -0500

    Conditional Statements - Lecture 6 + Practice Problems

---
+++ ch03/conditional_one.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+}
+
+
+
+// int main() {
+//     int floor, actual_floor;
+
+//     cout << "Enter desired floor number: ";
+//     cin >> floor;
+
+//     actual_floor = floor;
+
+//     if (floor > 13) { // We don't like Floor 13 it is scary
+//         actual_floor--; // decrement by 1
+//     }
+//     cout << "You are going to floor " << actual_floor << endl;
+//     return 0;
+// }
---
+++ ch03/conditional_practice_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// Question 1
+
+// Question 2
+
+// Question 3
+
+// Question 4
