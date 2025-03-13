b99ff8afb660900ae551e8efc0cb50f6d927041b
Keshawn <skeshawn86@gmail.com>
2025-03-13 09:18:52 -0400

    Add richter scale problem - in class example

---
+++ ch03/richter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double richter;
+    cout << "Please enter the richter scale value: ";
+    cin >> richter;
+
+    if (richter >= 8.0) {
+        cout << "Most structures fall" << endl;
+    } else if (richter >= 7.0) {
+        cout << "Many buildings destroyed" << endl;
+    } else if (richter >= 6.0) {
+        cout << "Many buildings considerably damaged, some collapse" << endl;
+    } else if (richter >= 4.5) {
+        cout << "Damage to poorly constructed buildings" << endl;
+    } else {
+        cout << "No destruction of buildings" << endl;
+    }
+}
